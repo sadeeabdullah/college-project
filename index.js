8c12f899311b61045a617cd911f54542c1cab975
sadeeabdullah <sadeeabdullah016@gmail.com>
2024-09-23 13:12:48 +0600

    changing on the feature list on home page?

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+
+
+
 // fetching data from the API
 let p = [];
 const fetchPromises = [];
@@ -28,6 +31,8 @@ Promise.all(fetchPromises).then(() => {
     document.getElementById('loader').style.display = 'none';
 
     displayProducts(p); // Call the function to render the product cards
+
+    displayProductsOnHome(p.slice(1,3))
 });
 
 // Adding a cart variable so that I can add items to the cart later
@@ -43,14 +48,16 @@ function displayProducts(products) {
         card.classList.add('card');
         
         card.innerHTML = `
-            <div class="p-3 hover:scale-[1.01] duration-500 hover:shadow-lg mx-auto rounded-md border-gray-300 border bg-[#e2e3e4] w-[300px] shadow-md">
-                <img class="rounded-t-sm w-full duration-500 object-cover hover:scale-105 h-[250px]" src="${product.image}" alt="${product.title}">
+            <div class="p-3 hover:scale-[1.01] duration-500 hover:shadow-lg mx-auto rounded-md border-gray-300 border bg-[#e2e3e4] w-[300px] shadow-md ">
+                <div class="overflow-hidden ">
+                <img class="rounded-t-sm  w-full duration-500 object-cover  hover:scale-105 h-[250px]" src="${product.image}" alt="${product.title}">
+                </div>
                 <div class="flex flex-col gap-3 justify-between mt-2">
                     <div>
-                        <h1 class="text-lg font-semibold">${product.title.length > 14 
-                            ? product.title.slice(0, 14) + '...' 
-                            : product.title}</h1>
-                        <p class="font-semibold">Price: <span>${product.price} <span class="font-bold text-xl">$</span></span></p>
+                        <h1 class="text-md font-semibold">${product.title.length < 14 
+                            ?  product.title
+                            : product.title.slice(0, 25,) + '...'}</h1>
+                        <p class="font-normal text-sm">Price: <span>${product.price} <span class="font-thin ">$</span></span></p>
                     </div>
                     <div class="flex justify-between">
                         <button class="addToCartBtn" onclick="addToCart(${product.id})">Add to Cart</button>
@@ -60,6 +67,36 @@ function displayProducts(products) {
             </div>`;
         
         productsList.appendChild(card);
+    });
+}
+function displayProductsOnHome(products) {
+    const featureList = document.getElementById('feature-list');
+    featureList.innerHTML = '';
+
+    products.forEach(product => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        
+        card.innerHTML = `
+            <div class="p-3 hover:scale-[1.01] duration-500 hover:shadow-lg mx-auto rounded-md border-gray-300 border bg-[#e2e3e4] w-[300px] shadow-md ">
+                <div class="overflow-hidden ">
+                <img class="rounded-t-sm  w-full duration-500 object-cover  hover:scale-105 h-[250px]" src="${product.image}" alt="${product.title}">
+                </div>
+                <div class="flex flex-col gap-3 justify-between mt-2">
+                    <div>
+                        <h1 class="text-md font-semibold">${product.title.length < 14 
+                            ?  product.title
+                            : product.title.slice(0, 25,) + '...'}</h1>
+                        <p class="font-normal text-sm">Price: <span>${product.price} <span class="font-thin ">$</span></span></p>
+                    </div>
+                    <div class="flex justify-between">
+                        <button class="addToCartBtn" onclick="addToCart(${product.id})">Add to Cart</button>
+                        <button class="buyBtn">Buy Now</button>
+                    </div>
+                </div>
+            </div>`;
+        
+            featureList.appendChild(card);
     });
 }
 
